e5ca347c718cc4fe1494a6fbd3dc6df9fa660bf2
Malware Utkonos <utkonos@malwarolo.gy>
2022-05-23 12:47:19 -0400

    Move package_data to setup.cfg

--- setup.py
+++ setup.py
@@ -12,7 +12,6 @@ https://github.com/pypa/sampleproject
 import setuptools
 
 setuptools.setup(
-    package_data={'oscg': ['templates/*.xml']},
     python_requires='>=3.10',
     entry_points={
         'console_scripts': [
